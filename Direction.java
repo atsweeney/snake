138fa2f7e743672961dcfa2a1c6f9dc65b395929
wayfrae <frazier.zach@gmail.com>
2017-11-17 13:28:20 -0700

    Update Direction.java

--- Direction.java
+++ Direction.java
@@ -7,8 +7,8 @@ package snake;
  * Assignment: Group Project
  */
 public enum Direction {
-	RIGHT(90),  //Movement will occur to the right
-	UP(360),		//Movement will occur in the upward direction
+	RIGHT(90),	//Movement will occur to the right
+	UP(360),	//Movement will occur in the upward direction
 	LEFT(270),	//Movement will occur to the left
 	DOWN(180);	//Movement will occur in the downward direction
 	
